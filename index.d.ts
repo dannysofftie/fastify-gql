340976d5df40c9597b01ea8e8f394bde4f648770
dannysofftie <dankim761@gmail.com>
2020-10-01 15:19:05 +0300

    Update typings, prefer to use .env.sample

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client';
-import { IConfigs } from '../src/configs';
-import { IMQTTUtilities } from '../src/libraries/Mqtt';
-import { IPlugins } from '../src/plugins';
-import { IJWTPayload } from '../src/plugins/Token';
+import { IConfigs } from './src/configs';
+import { IMQTTUtilities } from './src/libraries/Mqtt';
+import { IPlugins } from './src/plugins';
+import { IJWTPayload } from './src/plugins/Token';
 
 declare module 'fastify' {
     interface FastifyInstance {
         mqtt: IMQTTUtilities;
         plugins: IPlugins;
         configs: IConfigs;
+        mongouri: string;
     }
 
     interface IRequestContext {
